12ba21f34cd70055f04f68320cb6547d6a9d2ed2
goubamoussa <133861194+goubamoussa@users.noreply.github.com>
2023-08-21 14:32:52 +0000

    seed de la base de donne

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence(rand(2, 7));
+        $datetime = $this->faker->dateTimeBetween(startDate: '-1 month', endDate: 'now');
+        $content = '';
+        for ($i = 0; $i < 5; $i++) {
+            $content = '<p class="mb-4">' . $this->faker->sentence(rand(2, 10)) . '</p>';
+        }
+        return [
+
+            'title' => $title,
+            'slug' =>  Str::slug($title) . '-' . rand(1111,  9999),
+            'compagny' => $this->faker->company,
+            'location' => $this->faker->country,
+            'logo' => basename($this->faker->image(storage_path('app/public'))),
+            'tres_recherche' => (rand(1, 9) > 7),
+            'est_active' => true,
+            'content' => $content,
+            'lien' => $this->faker->url,
+            'created_at' => $datetime,
+            'updated_at' => $datetime,
+            'user_id' => \App\Models\User::inRandomOrder()->first()->id,
+        ];
+    }
+}
---
+++ database/factories/TagFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+
+
+    public function definition()
+    {
+        $name = ucwords($this->faker->word);
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+        ];
+    }
+}
