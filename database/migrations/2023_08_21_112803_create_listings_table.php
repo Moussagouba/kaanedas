74aafff42307f9ef7812dabc454622273bbda670
goubamoussa <133861194+goubamoussa@users.noreply.github.com>
2023-08-21 12:10:12 +0000

    creation du projet

---
+++ database/migrations/2023_08_21_112803_create_listings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('compagny');
+            $table->string('location');
+            $table->string('logo')->nullable();
+            $table->boolean('tres_recherche')->default(false);
+            $table->boolean('est_active')->default(true);
+            $table->text('content');
+            $table->string('lien');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+}
